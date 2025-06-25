7864b62096cf8300d13b5cbaa4c86e5305603fb6
SayanH-BIT <sayan.haldar.brainium@gmail.com>
2025-06-25 17:41:45 +0530

    Login & Registration page modified and API page created 📝

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,31 +1,65 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../App.css';
 import Header1 from '../components/Header1';
 import { useNavigate } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import { BASE_URL } from '../components/API';
+import axios from 'axios';
+
 
 const Login = () => {
     const navigate = useNavigate();
-    const handleRegClick = () => {
-        navigate('/register')
-    }
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const values = {
+            email: formData.get("email"),
+            password: formData.get("password"),
+        };
+        try {
+            const { data } = await axios.post(`${BASE_URL}/login`, values);
+            toast.success("Logged in Successfully!", {
+                // autoClose: 2000,
+                position: "top-center",
+                pauseOnHover: false,
+                onClose: () => navigate('/'),
+            });
+            localStorage.setItem('user', JSON.stringify({ ...data, password: "" }));
+        } catch (error) {
+            toast.error("Invalid credentials. Please try again.", {
+                position: "top-center"
+            });
+            console.log(error);
+        }
+    };
+    // prevent for login user & protect the route
+    useEffect(() => {
+        const users = localStorage.getItem("user");
+        console.log('users====>', users);
+        if (users) {
+            navigate('/');
+        }
+    }, [navigate]);
+
     return (
         <>
             <Header1 />
+            <ToastContainer />
             <div className="login-page">
                 <h2 className='welcome'>Welcome Back</h2>
-                <form action="" className="login-form">
+                <form className="login-form" onSubmit={handleLogin}>
                     <div className='login-form-attributes'>
-                        <h4>Email Address</h4>
-                        <input className='login-form-input' placeholder='Enter username' />
+                        <label htmlFor='email'>Email Address</label>
+                        <input type='email' className='login-form-input' id='email' name='email' placeholder='Enter email' required />
                     </div>
                     <div className='login-form-attributes'>
-                        <h4>Password</h4>
-                        <input className='login-form-input' placeholder='Enter password' />
+                        <label htmlFor="password">Password</label>
+                        <input type='password' className='login-form-input' name='password' id='password' placeholder='Enter password' required />
                     </div>
-                    <button type='button' className='login-btn'>Login</button>
+                    <button type='submit' className='login-btn'>Login</button>
                     <div className='signup-section'>
                         <p>Don't have an Account? </p>
-                        <h3 style={{color:'blueviolet', cursor:"pointer"}} onClick={handleRegClick}>Sign Up</h3>
+                        <p style={{ fontSize: "1.2rem", fontWeight: "700", color: 'blueviolet', cursor: "pointer" }} onClick={() => navigate('/register')}>Sign Up</p>
                     </div>
                 </form>
             </div>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,46 +1,94 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header1 from '../components/Header1';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { toast, ToastContainer } from 'react-toastify';
+import { BASE_URL } from '../components/API';
 
 const Register = () => {
     const navigate = useNavigate();
-    const handleClick = () => {
-        navigate('/login');
+
+    // registration form handler
+    const handleRegistration = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const values = {
+            fullname: formData.get("fullname"),
+            contact: formData.get("contact"),
+            email: formData.get("email"),
+            password: formData.get("password"),
+            confirmPassword: formData.get("re-password"),
+            terms: formData.get("terms"),
+        };
+        const { password, confirmPassword } = values;
+        if (password !== confirmPassword) {
+            toast.error("password mismatched!")
+            return
+        };
+        try {
+            await axios.post(`${BASE_URL}/register`, values, {
+                headers: { "Content-Type": "application/json" }
+            });
+            toast.success("Registration Successfull!")
+            navigate("/login");
+        } catch (error) {
+            toast.error("Check your data properly.")
+            console.log("Error during registration - ", error);
+        }
+        console.log('form-values==>', values);
     };
+
+    // prevent for logged in user
+    useEffect(()=>{
+        if(localStorage.getItem("user")){
+            navigate("/login")
+        }
+    }, [navigate]);
+
     return (
         <>
             <Header1 />
+            <ToastContainer autoClose={2000} position="top-center" pauseOnHover={false} />
             <div className="register-page">
                 <h2 className='welcome'>New Registration</h2>
-                <form action="" className="register-form">
+                <form className="register-form" method='post' onSubmit={handleRegistration}>
                     <div style={{ display: "flex", gap: "2rem", width: "88%" }}>
                         <div className='register-form-attributes'>
-                            <h4>Full Name</h4>
-                            <input className='register-form-input' placeholder='Enter your fullname' />
+                            <label htmlFor='fullname'>Full Name</label>
+                            <input type='text' className='register-form-input' name='fullname' id='fullname' placeholder='Enter your fullname' required />
                         </div>
                         <div className='register-form-attributes'>
-                            <h4>Contact Number</h4>
-                            <input className='register-form-input' placeholder='Enter mobile number' />
+                            <label htmlFor='contact'>Contact Number</label>
+                            <input type='tel' className='register-form-input' name='contact' id='contact' placeholder='Enter mobile number' required />
                         </div>
                     </div>
                     <div className='register-form-attributes'>
-                        <h4>Email Address</h4>
-                        <input className='register-form-input' placeholder='Enter email id' />
+                        <label htmlFor='email'>Email Address</label>
+                        <input type='email' className='register-form-input' name='email' id='email' placeholder='Enter email id' required />
                     </div>
                     <div style={{ display: "flex", gap: "2rem", width: "88%" }}>
                         <div className='register-form-attributes'>
-                            <h4>Password</h4>
-                            <input className='register-form-input' placeholder='Enter password' />
+                            <label htmlFor='password'>Password</label>
+                            <input type='password' className='register-form-input' name='password' id='password' placeholder='Enter password' required />
                         </div>
                         <div className='register-form-attributes'>
-                            <h4>Confirm Password</h4>
-                            <input className='register-form-input' placeholder='Re-enter password' />
+                            <label htmlFor='re-password'>Confirm Password</label>
+                            <input type='password' className='register-form-input' name='re-password' id='re-password' placeholder='Re-enter password' required />
                         </div>
                     </div>
-                    <button type='button' className='register-btn'>Create Account</button>
+                    <div style={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        gap: "0.4rem"
+                    }}>
+                        <input type='checkbox' defaultValue name='terms' id='terms' required />
+                        <label htmlFor="terms"> I agree with the <Link to='#'>terms & conditions</Link>.</label>
+                    </div>
+                    <button type='submit' className='register-btn'>Create Account</button>
                     <div className='signin-section'>
                         <p>Already have an Account? </p>
-                        <h3 style={{ color: 'blueviolet', cursor:"pointer" }} onClick={handleClick}>Sign In</h3>
+                        <p style={{ fontSize: "1.2rem", fontWeight: "700", color: 'blueviolet', cursor: "pointer" }} onClick={() => navigate('/login')}>Sign In</p>
                     </div>
                 </form>
             </div>
